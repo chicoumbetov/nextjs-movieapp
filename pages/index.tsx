1d09374beaf2403da76856f15085451f7e199ed3
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-01 10:47:03 +0200

    :construction: getStaticProps for get & pass data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from "next";
 import Hero from "../components/Hero";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ movies }: any) => {
+  console.log("movies : ", movies);
+
   return (
     <div>
       <Hero />
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const movies = [{ name: "Hello" }];
+
+  return {
+    props: { movies },
+  };
+}
 
 export default Home;
