f540e4f796a69ad5b82f60b308f8a1bbc3dbb907
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-01 09:56:37 +0200

    :construction: Hero component created

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="text-center bg-white pb-10">
+      <div className="w-60 mx-auto">
+        <Image src="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
