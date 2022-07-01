8b121757c6935d6af03e5830880c0f028c1de587
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-01 10:18:34 +0200

    :lipstick: contact page created

---
+++ pages/contact.tsx
@@ -0,0 +1,9 @@
+const contact = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">contact us</h1>
+    </div>
+  );
+};
+
+export default contact;
