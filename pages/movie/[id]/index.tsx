c8580e54894983fa58b4720c072c1d362f0a4c51
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-01 13:57:46 +0200

    :construction: MovieDetail fetch + UI

--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -1,5 +1,40 @@
-const Movie = () => {
-  return <div>Movie Detail</div>;
+import axios from "axios";
+import { MovieType } from "../../../components/interfaces";
+import { server } from "../../../config";
+
+const Movie = ({ movie }: { movie: MovieType }) => {
+  console.log("detail : ", movie);
+
+  return <div>{movie.title}</div>;
 };
+
+export async function getStaticProps(context: any) {
+  const { id } = context.params;
+
+  const res = await axios(
+    `${server}/${id}?api_key=${process.env.API_KEY}&language=en-US&page=1`
+  );
+
+  const movie = res.data;
+
+  return {
+    props: { movie },
+  };
+}
+
+export async function getStaticPaths() {
+  const res = await axios(
+    `${server}/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+  );
+  const movies = res.data.results;
+
+  const ids = movies.map((m: MovieType) => m.id);
+  const paths = ids.map((id: string) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default Movie;
