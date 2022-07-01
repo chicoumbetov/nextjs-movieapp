911cc66f5db3f5b095053d687c75d74eb22c3ffb
shynggys <shynggys.umbetov@tkblueagency.eu>
2022-07-01 10:17:50 +0200

    :lipstick: Hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="text-center bg-white pb-10">
       <div className="w-60 mx-auto">
-        <Image src="" />
+        <Image
+          src={"/assets/home_cinema.png"}
+          height={200}
+          width={200}
+          layout="responsive"
+          alt="home cinema"
+        />
+        <h1 className="text-2xl text-gray-700 uppercase font-bold">
+          Welcome to WatchMe
+        </h1>
+        <p className="text-gray-500">Produce FILM feature, TV & Game</p>
+        <Link href="/contact">
+          <button className="bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4">
+            CONTACT US
+          </button>
+        </Link>
       </div>
     </div>
   );
